7f6e10dcaaa108e0b9be92b4244f0322c2d81e04
nawranaz <nawranazli08@gmail.com>
2025-03-16 12:10:35 +0700

    Menambahkan class DataGeneric dengan method PrintData

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        DataGeneric<string> nimData = new DataGeneric<string>("103022300054");
+
+        nimData.PrintData();
+    }
+}
